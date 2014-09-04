26d47d14795079f3e92b03f99ccbb3122d980b8e
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-04 14:34:16 +0200

    Added skeleton methods for reading and writing processes.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Process.java
+++ eclipse/src/duro/runtime/Process.java
@@ -1,5 +1,11 @@
 package duro.runtime;
 
 public class Process {
+	public static Process read(String path) {
+		return null;
+	}
+	
+	public static void write(String path, Process process) {
 
+	}
 }
