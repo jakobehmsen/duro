45c51896d2a2fce26e71857f9578edbe77d4bea1
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-04 14:34:39 +0200

    Added initial driver logic into Main.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/Main.java
+++ eclipse/src/duro/Main.java
@@ -1,7 +1,14 @@
 package duro;
 
+import duro.runtime.Runtime;
+
 public class Main {
 	public static void main(String[] args) {
-		// Dummy comment
+		// Given a path (args[0]), continue the process at that path.
+		
+		String path = args[0];
+		duro.runtime.Process process = duro.runtime.Process.read(path);
+		duro.runtime.Runtime runtime = new Runtime();
+		runtime.run(process);
 	}
 }
