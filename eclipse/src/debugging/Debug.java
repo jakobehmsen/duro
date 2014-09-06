1ff159da10ecdeaaf7f5e125c662323e4f71535f
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-06 11:02:00 +0200

    Added debugging facilities to support filtering out debugging messages.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/debugging/Debug.java
@@ -0,0 +1,15 @@
+package debugging;
+
+public class Debug {
+	public static final int LEVEL_LOW = 0;
+	public static final int LEVEL_HIGH = 1;
+	
+	private static final int maxLevel = LEVEL_LOW;
+//	private static final int maxLevel = LEVEL_HIGH;
+	
+	public static final void println(int level, String message) {
+		if(level <= maxLevel) {
+			System.out.println("DEBUG: " + message);
+		}
+	}
+}
