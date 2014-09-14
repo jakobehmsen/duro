14c22d52681dcf6473eba9a980e2a3b7b311ec2c
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-14 08:48:56 +0200

    Added opcode for remainder instructions.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Instruction.java
+++ eclipse/src/duro/runtime/Instruction.java
@@ -58,6 +58,7 @@ public class Instruction implements Serializable {
 	public static final int OPCODE_SP_SUB = 74;
 	public static final int OPCODE_SP_MULT = 75;
 	public static final int OPCODE_SP_DIV = 76;
+	public static final int OPCODE_SP_REM = 77;
 
 	public static final int OPCODE_SP_WRITE = 127;
 	public static final int OPCODE_SP_NEXT_LINE = 128;
