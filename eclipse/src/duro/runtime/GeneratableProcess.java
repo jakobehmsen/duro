e0b96246a7e4f2d73bd32b601fe0a43ff57e3f51
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-14 20:31:00 +0200

    Added self as parameter for generatable processes, which is used to create generators.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/GeneratableProcess.java
+++ eclipse/src/duro/runtime/GeneratableProcess.java
@@ -7,10 +7,12 @@ public class GeneratableProcess extends DictionaryProcess {
 	private static final long serialVersionUID = 1L;
 	
 	private CallFrameInfo callFrameInfo;
+	private Process self;
 	private Object[] arguments;
 
-	public GeneratableProcess(CallFrameInfo callFrameInfo, Object[] arguments) {
+	public GeneratableProcess(CallFrameInfo callFrameInfo, Process self, Object[] arguments) {
 		this.callFrameInfo = callFrameInfo;
+		this.self = self;
 		this.arguments = arguments;
 	}
 
@@ -27,6 +29,7 @@ public class GeneratableProcess extends DictionaryProcess {
 			
 			Instruction[] instructions = new Instruction[] {
 				new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
+				new Instruction(Instruction.OPCODE_LOAD_ANY, self),
 				new Instruction(Instruction.OPCODE_LOAD_ARRAY, arguments),
 				new Instruction(Instruction.OPCODE_SP_NEW_GENERATOR),
 				new Instruction(Instruction.OPCODE_RET, 1)
