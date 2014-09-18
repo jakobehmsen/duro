c5d63a2258e1cc5effe9384b148016bff570ea3a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-18 09:26:47 +0200

    Changed creation of call frame info for next messages to be done once in generator.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/GeneratorProcess.java
+++ eclipse/src/duro/runtime/GeneratorProcess.java
@@ -10,28 +10,32 @@ public class GeneratorProcess extends DictionaryProcess {
 	
 	private CustomProcess.Frame frame;
 
+	private final CallFrameInfo nextCallFrameInfo;
+
 	public GeneratorProcess(Frame frame) {
 		// -1 because resume increments instruction pointer
 		frame.instructionPointer = -1;
 		this.frame = frame;
+		
+		Instruction[] nextInstructions = new Instruction[] {
+			new Instruction(Instruction.OPCODE_LOAD_FRAME, frame),
+			new Instruction(Instruction.OPCODE_RESUME),
+			new Instruction(Instruction.OPCODE_RET_FORWARD),
+		};
+		nextCallFrameInfo = new CallFrameInfo(0, 0, nextInstructions);
 	}
 
 	@Override
 	public Object getCallable(Object key) {
-		if(key.equals("next")) {
-			Instruction[] instructions = new Instruction[] {
-				new Instruction(Instruction.OPCODE_LOAD_FRAME, frame),
-				new Instruction(Instruction.OPCODE_RESUME),
-				new Instruction(Instruction.OPCODE_RET_FORWARD),
-			};
-			return new CallFrameInfo(0, 0, instructions);
-		}
+		if(key.equals("next"))
+			return nextCallFrameInfo;
 		
 		return super.getCallable(key);
 	}
 	
 	@Override
 	public DictionaryProcess newBase() {
+		// How to clone frame?
 		return new GeneratorProcess(frame);
 	}
 }
