82216b7f2ae6ca28ee34a4e097b6922973a51843
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-18 09:26:06 +0200

    Changed creation of call frame info for call messages to be done once in closure.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/ClosureProcess.java
+++ eclipse/src/duro/runtime/ClosureProcess.java
@@ -9,24 +9,27 @@ public class ClosureProcess extends DictionaryProcess {
 	private static final long serialVersionUID = 1L;
 	private CustomProcess.Frame frame;
 	private CallFrameInfo callFrameInfo;
+	
+	private CallFrameInfo callCallFrameInfo;
 
 	public ClosureProcess(Frame frame, CallFrameInfo callFrameInfo) {
 		this.frame = frame;
 		this.callFrameInfo = callFrameInfo;
+		
+		Instruction[] callInstructions = new Instruction[] {
+			new Instruction(Instruction.OPCODE_LOAD_FRAME, frame),
+			new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
+			new Instruction(Instruction.OPCODE_EXEC_ON_FRAME),
+			new Instruction(Instruction.OPCODE_RET, 1)
+		};
+		
+		callCallFrameInfo = new CallFrameInfo(0, 0, callInstructions);
 	}
 
 	@Override
 	public Object getCallable(Object key) {
-		if(key.equals("call")) {
-			Instruction[] instructions = new Instruction[] {
-				new Instruction(Instruction.OPCODE_LOAD_FRAME, frame),
-				new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
-				new Instruction(Instruction.OPCODE_EXEC_ON_FRAME),
-				new Instruction(Instruction.OPCODE_RET, 1)
-			};
-
-			return new CallFrameInfo(0, 0, instructions);
-		}
+		if(key.equals("call"))
+			return callCallFrameInfo;
 		
 		return super.getCallable(key);
 	}
