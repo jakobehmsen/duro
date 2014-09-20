8a918d21a3d51352f2bf1397cfa0b933a632c93f
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 19:57:14 +0200

    Added opcode for LOAD_BEHAVIOR instructions.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Instruction.java
+++ eclipse/src/duro/runtime/Instruction.java
@@ -47,6 +47,7 @@ public class Instruction implements Serializable {
 	public static final int OPCODE_LOAD_ANY = 34;
 	public static final int OPCODE_LOAD_THIS_FRAME = 35;
 	public static final int OPCODE_LOAD_FRAME = 36;
+	public static final int OPCODE_LOAD_BEHAVIOR = 37;
 
 	public static final int OPCODE_SP_OR = 65;
 	public static final int OPCODE_SP_AND = 66;
