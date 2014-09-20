afa36c4b6c7f5bbc7c3c2776587a71d2438ca3b0
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 20:20:26 +0200

    Replaced usage of CallFrameInfo with BehaviorProcess.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/GeneratableProcess.java
+++ eclipse/src/duro/runtime/GeneratableProcess.java
@@ -6,37 +6,51 @@ public class GeneratableProcess extends DictionaryProcess {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private CallFrameInfo callFrameInfo;
+	private BehaviorProcess behavior;
 	private Process self;
 	private Object[] arguments;
 	
-	private final CallFrameInfo iteratorCallFrameInfo;
+	private BehaviorProcess iteratorBehavior;
 
-	public GeneratableProcess(CallFrameInfo callFrameInfo, Process self, Object[] arguments) {
-		this.callFrameInfo = callFrameInfo;
+	public GeneratableProcess(BehaviorProcess behavior, Process self, Object[] arguments) {
+		this.behavior = behavior;
 		this.self = self;
 		this.arguments = arguments;
 		
-		Instruction[] iteratorInstructions = new Instruction[] {
-			new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
-			new Instruction(Instruction.OPCODE_LOAD_ANY, self),
-			new Instruction(Instruction.OPCODE_LOAD_ARRAY, arguments),
-			new Instruction(Instruction.OPCODE_SP_NEW_GENERATOR),
-			new Instruction(Instruction.OPCODE_RET, 1)
-		};
-		iteratorCallFrameInfo = new CallFrameInfo(0, 0, iteratorInstructions);
+//		Instruction[] iteratorInstructions = new Instruction[] {
+//			new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
+//			new Instruction(Instruction.OPCODE_LOAD_ANY, self),
+//			new Instruction(Instruction.OPCODE_LOAD_ARRAY, arguments),
+//			new Instruction(Instruction.OPCODE_SP_NEW_GENERATOR),
+//			new Instruction(Instruction.OPCODE_RET, 1)
+//		};
+//		iteratorCallFrameInfo = new CallFrameInfo(0, 0, iteratorInstructions);
 	}
 
 	@Override
 	public Object getCallable(ProcessFactory factory, Object key) {
-		if(key.equals("iterator"))
-			return iteratorCallFrameInfo;
+		if(key.equals("iterator")) {
+//			return iteratorCallFrameInfo;
+
+			if(iteratorBehavior == null) {
+				Instruction[] iteratorInstructions = new Instruction[] {
+					new Instruction(Instruction.OPCODE_LOAD_BEHAVIOR, behavior),
+					new Instruction(Instruction.OPCODE_LOAD_ANY, self),
+					new Instruction(Instruction.OPCODE_LOAD_ARRAY, arguments),
+					new Instruction(Instruction.OPCODE_SP_NEW_GENERATOR),
+					new Instruction(Instruction.OPCODE_RET, 1)
+				};
+				
+				iteratorBehavior = factory.createBehavior(new CallFrameInfo(0, 0, iteratorInstructions));
+			}
+			return iteratorBehavior;
+		}
 		
 		return super.getCallable(factory, key);
 	}
 	
 	@Override
 	public DictionaryProcess newBase() {
-		return new GeneratableProcess(callFrameInfo, self, arguments);
+		return new GeneratableProcess(behavior, self, arguments);
 	}
 }
