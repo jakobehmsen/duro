db1c11f3b922de4e9c7ba0440e6acd7dc5e2b54a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 20:02:46 +0200

    Changed ClosureProcess to use BehaviorProcess instead of CallFrameInfo.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/ClosureProcess.java
+++ eclipse/src/duro/runtime/ClosureProcess.java
@@ -6,34 +6,36 @@ public class ClosureProcess extends DictionaryProcess {
 	 */
 	private static final long serialVersionUID = 1L;
 	private FrameProcess frame;
-	private CallFrameInfo callFrameInfo;
+	private BehaviorProcess behavior;
 	
-	private CallFrameInfo callCallFrameInfo;
+	private BehaviorProcess callBehavior;
 
-	public ClosureProcess(FrameProcess frame, CallFrameInfo callFrameInfo) {
+	public ClosureProcess(FrameProcess frame, BehaviorProcess behavior) {
 		this.frame = frame;
-		this.callFrameInfo = callFrameInfo;
-		
-		Instruction[] callInstructions = new Instruction[] {
-			new Instruction(Instruction.OPCODE_LOAD_THIS_FRAME, frame),
-			new Instruction(Instruction.OPCODE_LOAD_FUNC, callFrameInfo),
-			new Instruction(Instruction.OPCODE_EXEC_ON_FRAME),
-			new Instruction(Instruction.OPCODE_RET, 1)
-		};
-		
-		callCallFrameInfo = new CallFrameInfo(0, 0, callInstructions);
+		this.behavior = behavior;
 	}
 
 	@Override
-	public Object getCallable(Object key) {
-		if(key.equals("call"))
-			return callCallFrameInfo;
+	public Object getCallable(ProcessFactory factory, Object key) {
+		if(key.equals("call")) {
+			if(callBehavior == null) {
+				Instruction[] callInstructions = new Instruction[] {
+					new Instruction(Instruction.OPCODE_LOAD_THIS_FRAME, frame),
+					new Instruction(Instruction.OPCODE_LOAD_BEHAVIOR, behavior),
+					new Instruction(Instruction.OPCODE_EXEC_ON_FRAME),
+					new Instruction(Instruction.OPCODE_RET, 1)
+				};
+				
+				callBehavior = factory.createBehavior(new CallFrameInfo(0, 0, callInstructions));
+			}
+			return callBehavior;
+		}
 		
-		return super.getCallable(key);
+		return super.getCallable(factory, key);
 	}
 	
 	@Override
 	public DictionaryProcess newBase() {
-		return new ClosureProcess(frame, callFrameInfo);
+		return new ClosureProcess(frame, behavior);
 	}
 }
