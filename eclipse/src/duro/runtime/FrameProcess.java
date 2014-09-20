904245497fb3dc795b95ee2d77f7bbc04840cc5f
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 10:15:56 +0200

    Added support for sender messages in frame process. Added implementation of newBase in frame process.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/FrameProcess.java
+++ eclipse/src/duro/runtime/FrameProcess.java
@@ -8,9 +8,31 @@ public class FrameProcess extends DictionaryProcess {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private CustomProcess.Frame frame;
+	public CustomProcess.Frame frame;
+	
+	private final CallFrameInfo outerCallFrameInfo;
 
 	public FrameProcess(Frame frame) {
 		this.frame = frame;
+		
+		Instruction[] iteratorInstructions = new Instruction[] {
+			new Instruction(Instruction.OPCODE_LOAD_THIS),
+			new Instruction(Instruction.OPCODE_SP_REIFIED_FRAME_SENDER),
+			new Instruction(Instruction.OPCODE_RET, 1)
+		};
+		outerCallFrameInfo = new CallFrameInfo(0, 0, iteratorInstructions);
+	}
+
+	@Override
+	public Object getCallable(Object key) {
+		if(key.equals("sender"))
+			return outerCallFrameInfo;
+		
+		return super.getCallable(key);
+	}
+	
+	@Override
+	public DictionaryProcess newBase() {
+		return new FrameProcess(frame);
 	}
 }
