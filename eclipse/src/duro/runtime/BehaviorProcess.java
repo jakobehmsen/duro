5f1f99ad875e94f43a472f0f22434b88016e8c12
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 20:50:27 +0200

    Changed behavior process to require the fields contained within a CallFrameInfo rather than a CallFrameInfo. I.e. parameterCount, variableCount, and instructions.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/BehaviorProcess.java
+++ eclipse/src/duro/runtime/BehaviorProcess.java
@@ -5,14 +5,20 @@ public class BehaviorProcess extends DictionaryProcess {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public CallFrameInfo callFrameInfo;
-
-	public BehaviorProcess(CallFrameInfo callFrameInfo) {
-		this.callFrameInfo = callFrameInfo;
-	}
+	public int parameterCount;
+	public int variableCount;
+	public Instruction[] instructions;
 	
+	public BehaviorProcess(int parameterCount, int variableCount, Instruction[] instructions) {
+		this.parameterCount = parameterCount;
+		this.variableCount = variableCount;
+		this.instructions = instructions;
+	}
+
+
+
 	@Override
 	public DictionaryProcess newBase() {
-		return new BehaviorProcess(callFrameInfo);
+		return new BehaviorProcess(parameterCount, variableCount, instructions);
 	}
 }
