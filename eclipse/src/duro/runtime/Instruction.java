3789813aecee377b6f93c4015dc5d1afb514d0c7
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-20 19:25:08 +0200

    Renamed opcode LOAD_REIFIED_FRAME to LOAD_THIS_REIFIED_FRAME. Added opcode for LOAD_REIFIED_FRAME instructions.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Instruction.java
+++ eclipse/src/duro/runtime/Instruction.java
@@ -46,7 +46,8 @@ public class Instruction implements Serializable {
 	public static final int OPCODE_LOAD_FRAME = 33;
 	public static final int OPCODE_LOAD_ARRAY = 34;
 	public static final int OPCODE_LOAD_ANY = 35;
-	public static final int OPCODE_LOAD_REIFIED_FRAME = 36;
+	public static final int OPCODE_LOAD_THIS_REIFIED_FRAME = 36;
+	public static final int OPCODE_LOAD_REIFIED_FRAME = 37;
 
 	public static final int OPCODE_SP_OR = 65;
 	public static final int OPCODE_SP_AND = 66;
