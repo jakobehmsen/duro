91090ddae1f53d0d31505b26d1f0a0773ff0462b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-21 11:11:52 +0200

    Removed redundant implementation of getCallable in FrameProcess.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/FrameProcess.java
+++ eclipse/src/duro/runtime/FrameProcess.java
@@ -9,29 +9,9 @@ public class FrameProcess extends DictionaryProcess {
 	private static final long serialVersionUID = 1L;
 
 	public CustomProcess.Frame frame;
-	
-	private BehaviorProcess senderBehavior;
 
 	public FrameProcess(Frame frame) {
 		this.frame = frame;
-	}
-
-	@Override
-	public Object getCallable(ProcessFactory factory, Object key) {
-		if(key.equals("sender")) {
-			if(senderBehavior == null) {
-				Instruction[] senderInstructions = new Instruction[] {
-					new Instruction(Instruction.OPCODE_LOAD_THIS),
-					new Instruction(Instruction.OPCODE_SP_FRAME_SENDER),
-					new Instruction(Instruction.OPCODE_RET, 1)
-				};
-				
-				senderBehavior = factory.createBehavior(0, 0, senderInstructions);
-			}
-			return senderBehavior;
-		}
-		
-		return super.getCallable(factory, key);
 	}
 	
 	@Override
