39a04e324c4f403e8b33fb4a70df37ba1fda7b5b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-22 13:24:01 +0200

    Added BooleanProcess to represent first class boolean.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/runtime/BooleanProcess.java
@@ -0,0 +1,14 @@
+package duro.runtime;
+
+public class BooleanProcess extends DictionaryProcess {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public boolean value;
+
+	public BooleanProcess(boolean value) {
+		this.value = value;
+	}
+}
