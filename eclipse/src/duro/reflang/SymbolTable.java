0dda1bc8c82bec40ce6990b197de95567597677b
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-23 14:34:01 +0200

    Changed SymbolTable to be used for instantiation.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/SymbolTable.java
+++ eclipse/src/duro/reflang/SymbolTable.java
@@ -3,14 +3,14 @@ package duro.reflang;
 import java.util.Hashtable;
 
 public class SymbolTable {
-	private static Hashtable<String, Integer> idToCodeMap = new Hashtable<String, Integer>();
+	private Hashtable<String, Integer> idToCodeMap = new Hashtable<String, Integer>();
 	private static Hashtable<Integer, String> codeToIdMap = new Hashtable<Integer, String>();
 	
-	public static String getIdFromSymbolCode(int symbolCode) {
+	public String getIdFromSymbolCode(int symbolCode) {
 		return codeToIdMap.get(symbolCode);
 	}
 	
-	public static int getSymbolCodeFromId(String id) {
+	public int getSymbolCodeFromId(String id) {
 		Integer symbolCode = idToCodeMap.get(id);
 		
 		if(symbolCode == null) {
