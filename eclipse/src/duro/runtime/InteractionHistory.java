8c628f9dbf5e3aad7186faca17c7dabf971cdc1f
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-23 12:00:04 +0200

    Extended support for identifying interaction in InteractionHistory by support interaction ids. Added support for extracting new interaction history.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/InteractionHistory.java
+++ eclipse/src/duro/runtime/InteractionHistory.java
@@ -1,6 +1,8 @@
 package duro.runtime;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class InteractionHistory {
@@ -9,32 +11,47 @@ public class InteractionHistory {
 		 * 
 		 */
 		private static final long serialVersionUID = 1L;
+		public final String interfaceId;
 		public final Instruction instruction;
 		public final Object output;
 		
-		public Interaction(Instruction instruction, Object output) {
+		public Interaction(String interfaceId, Instruction instruction, Object output) {
+			this.interfaceId = interfaceId;
 			this.instruction = instruction;
 			this.output = output;
 		}
 	}
 	
-	private int index;
+	private int initialSize;
 	private List<Interaction> interactions;
+	private Hashtable<String, Integer> tracks = new Hashtable<String, Integer>();
 	
 	public InteractionHistory(List<Interaction> interactions) {
 		this.interactions = interactions;
+		initialSize = interactions.size();
+	}
+
+	public boolean hasNewInteractions() {
+		return initialSize < interactions.size();
 	}
 	
-	public void append(Instruction instruction, Object output) {
-		interactions.add(new Interaction(instruction, output));
-		index++;
+	public List<Interaction> getNewInteractions() {
+		return new ArrayList<Interaction>(interactions.subList(initialSize, interactions.size()));
 	}
 	
-	public Object nextOutputFor(int opcode) {
-		for(; index < interactions.size(); index++) {
-			Interaction interaction = interactions.get(index);
-			if(interaction.instruction.opcode == opcode)
+	public void append(String interfaceId, Instruction instruction, Object output) {
+		interactions.add(new Interaction(interfaceId, instruction, output));
+	}
+	
+	public Object nextOutputFor(String interfaceId, int opcode) {
+		tracks.putIfAbsent(interfaceId, 0);
+		int trackIndex = tracks.get(interfaceId);
+		for(int i = trackIndex; i < interactions.size(); i++) {
+			Interaction interaction = interactions.get(i);
+			if(interaction.instruction.opcode == opcode && interaction.interfaceId.equals(interfaceId)) {
+				tracks.put(interfaceId, trackIndex + 1);
 				return interaction.output;
+			}
 		}
 		
 		return null;
