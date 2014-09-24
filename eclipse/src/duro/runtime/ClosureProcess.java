19dee8b6c382d191350b702b9a82b1da5654688a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-24 08:58:39 +0200

    Removed implementation of getCallable in ClosureProcess.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/ClosureProcess.java
+++ eclipse/src/duro/runtime/ClosureProcess.java
@@ -5,35 +5,14 @@ public class ClosureProcess extends DictionaryProcess {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private FrameProcess frame;
-	private BehaviorProcess behavior;
-	private int[] ordinals;
-	
-	private BehaviorProcess callBehavior;
+	public final FrameProcess frame;
+	public final BehaviorProcess behavior;
+	public int[] ordinals;
 
 	public ClosureProcess(FrameProcess frame, BehaviorProcess behavior, int[] ordinals) {
 		this.frame = frame;
 		this.behavior = behavior;
 		this.ordinals = ordinals;
-	}
-
-	@Override
-	public Object getCallable(ProcessFactory factory, Object key) {
-		if(key.equals("call")) {
-			if(callBehavior == null) {
-				Instruction[] callInstructions = new Instruction[] {
-					new Instruction(Instruction.OPCODE_LOAD_FRAME, frame),
-					new Instruction(Instruction.OPCODE_LOAD_BEHAVIOR, behavior),
-					new Instruction(Instruction.OPCODE_EXEC_ON_FRAME, ordinals),
-					new Instruction(Instruction.OPCODE_RET, 1)
-				};
-				
-				callBehavior = factory.createBehavior(0, 0, callInstructions);
-			}
-			return callBehavior;
-		}
-		
-		return super.getCallable(factory, key);
 	}
 	
 	@Override
