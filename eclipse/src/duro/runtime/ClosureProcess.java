1d78523267a18e0a23acf561b4be17efd26a1b2c
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-25 14:23:04 +0200

    Changed closure process to use argument offset in closed frame and parameter count for calls on it.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/ClosureProcess.java
+++ eclipse/src/duro/runtime/ClosureProcess.java
@@ -7,16 +7,18 @@ public class ClosureProcess extends DictionaryProcess {
 	private static final long serialVersionUID = 1L;
 	public final FrameProcess frame;
 	public final BehaviorProcess behavior;
-	public int[] ordinals;
+	public final int argumentOffset;
+	public final int parameterCount;
 
-	public ClosureProcess(FrameProcess frame, BehaviorProcess behavior, int[] ordinals) {
+	public ClosureProcess(FrameProcess frame, BehaviorProcess behavior, int argumentOffset, int parameterCount) {
 		this.frame = frame;
 		this.behavior = behavior;
-		this.ordinals = ordinals;
+		this.argumentOffset = argumentOffset;
+		this.parameterCount = parameterCount;
 	}
 	
 	@Override
 	public DictionaryProcess newBase() {
-		return new ClosureProcess(frame, behavior, ordinals);
+		return new ClosureProcess(frame, behavior, argumentOffset, parameterCount);
 	}
 }
