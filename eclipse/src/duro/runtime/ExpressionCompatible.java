656054eb355cb16e09a37ed1cef2561d97288c6a
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-25 08:44:35 +0200

    Added annotations DoesReturn and ExpressionCompatible for annotating opcodes.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/runtime/ExpressionCompatible.java
@@ -0,0 +1,13 @@
+package duro.runtime;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ExpressionCompatible {
+	int operandCount() default 0;
+	int popCount() default 0;
+}
