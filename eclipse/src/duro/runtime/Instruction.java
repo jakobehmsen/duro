b6a760eb116747b88987e155347fda6062cfb894
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-28 13:05:12 +0200

    Modified opcodes SET, SET_PROTO, and GET to require an additional Integer operand for parameter counts.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Instruction.java
+++ eclipse/src/duro/runtime/Instruction.java
@@ -31,11 +31,11 @@ public class Instruction implements Serializable {
 	public static final int OPCODE_IF_TRUE = 18;
 	public static final int OPCODE_IF_FALSE = 19;
 	public static final int OPCODE_JUMP = 20;
-	@ExpressionCompatible(operandTypes = {String.class}, popCount = 2)
+	@ExpressionCompatible(operandTypes = {String.class, Integer.class}, popCount = 2)
 	public static final int OPCODE_SET = 21;
-	@ExpressionCompatible(operandTypes = {String.class}, popCount = 2)
+	@ExpressionCompatible(operandTypes = {String.class, Integer.class}, popCount = 2)
 	public static final int OPCODE_SET_PROTO = 22;
-	@ExpressionCompatible(operandTypes = {String.class}, popCount = 1)
+	@ExpressionCompatible(operandTypes = {String.class, Integer.class}, popCount = 1)
 	public static final int OPCODE_GET = 23;
 	@ExpressionCompatible(popCount = 3)
 	public static final int OPCODE_SLOTS_SET = 24;
