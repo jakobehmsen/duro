af1654d7a320b7bb438070b900efba192470512d
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-09-30 10:03:53 +0200

    Added extra debug level: medium.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/debugging/Debug.java
+++ eclipse/src/duro/debugging/Debug.java
@@ -2,9 +2,11 @@ package duro.debugging;
 
 public class Debug {
 	public static final int LEVEL_LOW = 0;
-	public static final int LEVEL_HIGH = 1;
+	public static final int LEVEL_MEDIUM = 1;
+	public static final int LEVEL_HIGH = 2;
 	
-	private static final int maxLevel = LEVEL_LOW;
+//	private static final int maxLevel = LEVEL_LOW;
+	private static final int maxLevel = LEVEL_MEDIUM;
 //	private static final int maxLevel = LEVEL_HIGH;
 	
 	public static final void println(int level, String message) {
