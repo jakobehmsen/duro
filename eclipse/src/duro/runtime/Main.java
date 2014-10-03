0e6fb603e65fccea1848fbf33627caed6470eeb4
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-03 08:33:42 +0200

    Added time taking and related debug console logging to runtime driver.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Main.java
+++ eclipse/src/duro/runtime/Main.java
@@ -6,6 +6,7 @@ import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.util.List;
 
+import duro.debugging.Debug;
 import duro.transcriber.Journal;
 
 public class Main {
@@ -18,23 +19,43 @@ public class Main {
 		String mainObjectCodePath = args[0];
 		String mainObjectCodeJournalPath = mainObjectCodePath + ".jnl";
 		try {
+			Debug.println(Debug.LEVEL_MEDIUM, "Reading main process...");
+			long startReadMainProcess = System.currentTimeMillis();
 			CustomProcess mainProcess;
 			
 			try (ObjectInput oo = new ObjectInputStream(new FileInputStream(mainObjectCodePath))) {
 				mainProcess = (CustomProcess) oo.readObject();
 		    }
-			
+			long endReadMainProcess = System.currentTimeMillis();
+			Debug.println(Debug.LEVEL_MEDIUM, "Read main process.");
+			Debug.println(Debug.LEVEL_MEDIUM, "Read main process time: " + (endReadMainProcess - startReadMainProcess));
+
+			Debug.println(Debug.LEVEL_MEDIUM, "Reading interaction history...");
+			long startReadInteractionHistory = System.currentTimeMillis();
 			Journal<InteractionHistory.Interaction> journal = Journal.read(mainObjectCodeJournalPath);
 			List<InteractionHistory.Interaction> commands = journal.getCommands();
+			long endReadInteractionHistory = System.currentTimeMillis();
 			
 			InteractionHistory interactionHistory = new InteractionHistory(commands);
+			Debug.println(Debug.LEVEL_MEDIUM, "Read interaction history.");
+			Debug.println(Debug.LEVEL_MEDIUM, "Read interaction history time: " + (endReadInteractionHistory - startReadInteractionHistory));
+
+			Debug.println(Debug.LEVEL_MEDIUM, "Running...");
+			long startEvaluation = System.currentTimeMillis();
 			mainProcess.replay(interactionHistory);
+			long endEvaluation = System.currentTimeMillis();
+			Debug.println(Debug.LEVEL_MEDIUM, "Ran.");
+			Debug.println(Debug.LEVEL_MEDIUM, "Ran time: " + (endEvaluation - startEvaluation));
+			
+			Debug.println(Debug.LEVEL_MEDIUM, "Logging interactions...");
+			long startLoggingInteractions = System.currentTimeMillis();
 			if(interactionHistory.hasNewInteractions())
 				journal.log(interactionHistory.getNewInteractions());
 			
-//			duro.runtime.Runtime runtime = new Runtime(journal);
-//			runtime.resume(mainProcess);
 			journal.close();
+			long endLoggingInteractions = System.currentTimeMillis();
+			Debug.println(Debug.LEVEL_MEDIUM, "Logged interactions.");
+			Debug.println(Debug.LEVEL_MEDIUM, "Logged interactions time: " + (endLoggingInteractions - startLoggingInteractions));
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
