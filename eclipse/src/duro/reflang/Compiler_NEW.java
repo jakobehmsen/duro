3d581c29a0a055a7755d48d0f51d0ac19b20c1c7
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-05 18:51:43 +0200

    Added primitive binding to while primitive.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/Compiler_NEW.java
+++ eclipse/src/duro/reflang/Compiler_NEW.java
@@ -124,6 +124,7 @@ public class Compiler_NEW {
 		Hashtable<Selector, PrimitiveVisitorFactory> primitiveMap = new Hashtable<Selector, PrimitiveVisitorFactory>();
 		primitiveMap.put(Selector.get("ifElse", 3), new PrimitiveVisitorFactory.IfElse());
 		primitiveMap.put(Selector.get("if", 2), new PrimitiveVisitorFactory.IfElse());
+		primitiveMap.put(Selector.get("while", 2), new PrimitiveVisitorFactory.While());
 		primitiveMap.put(Selector.get("write", 1), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_SP_WRITE), false));
 		primitiveMap.put(Selector.get("readln", 0), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_SP_NEXT_LINE), true));
 		primitiveMap.put(Selector.get("eval", 1), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_CALL_CLOSURE), true));
