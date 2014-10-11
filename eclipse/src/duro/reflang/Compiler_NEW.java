deadd66b1a3a978cdd930fe37b5cbd2999383df7
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-11 18:04:17 +0200

    Added primitive binding from pause to pause.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/Compiler_NEW.java
+++ eclipse/src/duro/reflang/Compiler_NEW.java
@@ -136,6 +136,7 @@ public class Compiler_NEW {
 		primitiveMap.put(Selector.get("not", 1), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_SP_BOOLEAN_NOT), true));
 		primitiveMap.put(Selector.get("write", 1), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_SP_WRITE), false));
 		primitiveMap.put(Selector.get("readln", 0), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_SP_NEXT_LINE), true));
+		primitiveMap.put(Selector.get("pause", 0), new PrimitiveVisitorFactory.ConstInstruction(new Instruction(Instruction.OPCODE_PAUSE), true));
 		primitiveMap.put(Selector.get("eval", 1), new PrimitiveVisitorFactory.Eval());
 		primitiveMap.put(Selector.get("evalWith", 2), new PrimitiveVisitorFactory.Eval());
 		primitiveMap.put(Selector.get("evalWith", 3), new PrimitiveVisitorFactory.Eval());
