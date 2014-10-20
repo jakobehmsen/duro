83ef494a5bfbd23dfdf9a9d28539adc43a4625b6
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-20 17:39:56 +0200

    Removed unused classes and interfaces: BodyVisitor, PrimitiveVisitor, and PrimitiveVisitorFactory.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/PrimitiveVisitor.java
+++
@@ -1,9 +0,0 @@
-package duro.reflang;
-
-import java.util.List;
-
-import org.antlr.v4.runtime.ParserRuleContext;
-
-public interface PrimitiveVisitor {
-	void visitPrimitive(String id, List<ParserRuleContext> args);
-}
