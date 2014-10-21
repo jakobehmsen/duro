1be71e16bee8b5641a2b190d8be6ca8508936c9d
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-21 20:29:49 +0200

    Did a bit of cleaning up.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/runtime/Processor.java
+++ eclipse/src/duro/runtime/Processor.java
@@ -1,8 +1,6 @@
 package duro.runtime;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
