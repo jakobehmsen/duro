d88dc88e2aadf3eb85f32e2b34837faa6fbf7c11
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-22 20:33:18 +0200

    Added LazyCloneProcess which is able to support local slots but also postpone the cloning of the prototype for inheriters.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/runtime/LazyCloneProcess.java
@@ -0,0 +1,55 @@
+package duro.runtime;
+
+import java.util.List;
+
+import duro.runtime.InteractionHistory.Interaction;
+
+public class LazyCloneProcess extends Process {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private LocalizableProcess protoOrInstance;
+
+	public LazyCloneProcess(LocalizableProcess protoOrInstance) {
+		this.protoOrInstance = protoOrInstance;
+	}
+
+	@Override
+	public void resume(List<Interaction> playedInstructions) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getCallable(int selectorCode) {
+		return protoOrInstance.getCallable(selectorCode);
+	}
+
+	@Override
+	public Process lookup(int selectorCode) {
+		return protoOrInstance.lookup(selectorCode);
+	}
+
+	@Override
+	public boolean isDefined(int code) {
+		return protoOrInstance.isDefined(code);
+	}
+
+	@Override
+	public String[] getNames() {
+		return protoOrInstance.getNames();
+	}
+
+	@Override
+	public void define(int selectorCode, Process value) {
+		protoOrInstance = protoOrInstance.getAsLocal();
+		protoOrInstance.define(selectorCode, value);
+	}
+
+	@Override
+	public void defineProto(int selectorCode, Process value) {
+		protoOrInstance = protoOrInstance.getAsLocal();
+		protoOrInstance.define(selectorCode, value);
+	}
+}
