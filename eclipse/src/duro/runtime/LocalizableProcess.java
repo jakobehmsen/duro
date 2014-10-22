152db82a8cc39d3d56484b6f395fb9ad24933220
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-22 20:31:37 +0200

    Added interface LocalizableProcess to represent processes that are able to return themselves in a localized form.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/runtime/LocalizableProcess.java
@@ -0,0 +1,10 @@
+package duro.runtime;
+
+public abstract class LocalizableProcess extends Process {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public abstract LocalizableProcess getAsLocal();
+}
