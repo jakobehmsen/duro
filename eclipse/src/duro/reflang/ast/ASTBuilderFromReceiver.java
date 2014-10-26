3bb2626f2f3ec32b8e18c441489feb2effa79ead
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-26 15:53:38 +0100

    Changed ASTBuilderFromReceiver to implicitly build with ASTImplicitReceiver.INSTANCE as receiver.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/ast/ASTBuilderFromReceiver.java
+++ eclipse/src/duro/reflang/ast/ASTBuilderFromReceiver.java
@@ -3,7 +3,8 @@ package duro.reflang.ast;
 public abstract class ASTBuilderFromReceiver implements ASTBuilder {
 	@Override
 	public AST build() {
-		return createBuilder(() -> ASTThis.INSTANCE).build();
+//		return createBuilder(() -> ASTThis.INSTANCE).build();
+		return createBuilder(() -> ASTImplicitReceiver.INSTANCE).build();
 	}
 	
 	public abstract ASTBuilder createBuilder(ASTBuilder receiver);
