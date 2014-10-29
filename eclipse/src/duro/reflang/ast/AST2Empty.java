4eb60a580908cc51bd1552b55e550299126559e2
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-29 17:55:14 +0100

    Added new empty AST.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/reflang/ast/AST2Empty.java
@@ -0,0 +1,17 @@
+package duro.reflang.ast;
+
+import java.io.IOException;
+
+import duro.io.TreeWriter;
+
+public class AST2Empty implements AST2 {
+	public static final AST2Empty INSTANCE = new AST2Empty();
+
+	@Override
+	public void accept(ASTVisitor2 visitor) { }
+
+	@Override
+	public void writeTo(TreeWriter writer) throws IOException {
+		writer.write("¤");
+	}
+}
