228aa6f090c4e7befde89430b36ee92e7c423ab5
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-29 17:54:15 +0100

    Added new dict AST.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/reflang/ast/AST2Dict.java
@@ -0,0 +1,59 @@
+package duro.reflang.ast;
+
+import java.io.IOException;
+
+import duro.io.TreeWriter;
+
+public class AST2Dict implements AST2 {
+	public static class Entry {
+		public final String id;
+		public final int type;
+		public final int arity;
+		public final AST2 value;
+		
+		public Entry(String id, int type, int arity, AST2 value) {
+			this.id = id;
+			this.type = type;
+			this.arity = arity;
+			this.value = value;
+		}
+	}
+	
+	public final AST2 prototype;
+	public final Entry[] entries;
+
+	public AST2Dict(AST2 prototype, Entry[] entries) {
+		this.prototype = prototype;
+		this.entries = entries;
+	}
+
+	@Override
+	public void accept(ASTVisitor2 visitor) {
+		visitor.visitDict(this);
+	}
+	
+	@Override
+	public void writeTo(TreeWriter writer) throws IOException {
+		writer.write("#[");
+		for(int i = 0; i < entries.length; i++) {
+			if(i > 0)
+				writer.writeln();
+			writer.write(entries[i].id);
+			writer.write("/");
+			writer.write(entries[i].arity);
+			switch(entries[i].type) {
+			case ASTSlotAssignment.TYPE_REGULAR:
+				writer.write(" = ");
+				break;
+			case ASTSlotAssignment.TYPE_PROTO:
+				writer.write(" ^= ");
+				break;
+			case ASTSlotAssignment.TYPE_QUOTED:
+				writer.write(" => ");
+				break;
+			}
+			entries[i].value.writeTo(writer);
+		}
+		writer.write("]");
+	}
+}
--- eclipse/src/duro/reflang/ast/ASTVisitor2.java
+++ eclipse/src/duro/reflang/ast/ASTVisitor2.java
@@ -4,4 +4,5 @@ public interface ASTVisitor2 {
 	void visitArray(AST2Array ast);
 	void visitBehavior(AST2Behavior ast);
 	void visitClosure(AST2Closure ast);
+	void visitDict(AST2Dict ast);
 }
