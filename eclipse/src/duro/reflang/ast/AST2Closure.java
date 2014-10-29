cd51298f68b1920d24adffc5a60383c5a951e990
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-29 17:43:28 +0100

    Added new closure AST.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

---
+++ eclipse/src/duro/reflang/ast/AST2Closure.java
@@ -0,0 +1,35 @@
+package duro.reflang.ast;
+
+import java.io.IOException;
+
+import duro.io.TreeWriter;
+
+public class AST2Closure implements AST2 {
+	public final int parameterOffset;
+	public final int parameterCount;
+	public final AST body;
+	
+	public AST2Closure(int parameterOffset, int parameterCount, AST body) {
+		this.parameterOffset = parameterOffset;
+		this.parameterCount = parameterCount;
+		this.body = body;
+	}
+
+	@Override
+	public void accept(ASTVisitor2 visitor) {
+		visitor.visitClosure(this);
+	}
+
+	@Override
+	public void writeTo(TreeWriter writer) throws IOException {
+		writer.write("{");
+		writer.write(parameterOffset);
+		writer.write(",");
+		writer.write(parameterCount);
+		writer.write("|");
+		writer.indentnl();
+		body.writeTo(writer);
+		writer.dedentnl();
+		writer.write("}");
+	}
+}
--- eclipse/src/duro/reflang/ast/ASTVisitor2.java
+++ eclipse/src/duro/reflang/ast/ASTVisitor2.java
@@ -3,4 +3,5 @@ package duro.reflang.ast;
 public interface ASTVisitor2 {
 	void visitArray(AST2Array ast);
 	void visitBehavior(AST2Behavior ast);
+	void visitClosure(AST2Closure ast);
 }
