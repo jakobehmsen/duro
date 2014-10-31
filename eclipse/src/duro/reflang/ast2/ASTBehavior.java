de922caddbeaf6f22d901be17631add1c0bd6de1
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-31 16:22:14 +0100

    Removed field localCount.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/ast2/ASTBehavior.java
+++ eclipse/src/duro/reflang/ast2/ASTBehavior.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 import duro.io.TreeWriter;
 
 public class ASTBehavior implements AST {
-	public final int localCount;
 	public final AST body;
 	
-	public ASTBehavior(int localCount, AST body) {
-		this.localCount = localCount;
+	public ASTBehavior(AST body) {
 		this.body = body;
 	}
 
@@ -21,8 +19,6 @@ public class ASTBehavior implements AST {
 	@Override
 	public void writeTo(TreeWriter writer) throws IOException {
 		writer.write("(");
-		writer.write(localCount);
-		writer.write("|");
 		writer.indentnl();
 		body.writeTo(writer);
 		writer.dedentnl();
