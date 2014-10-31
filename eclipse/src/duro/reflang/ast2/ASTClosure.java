f7fb16157d7463b035768f3f70621cab4b2c6479
Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>
2014-10-31 16:22:42 +0100

    Removed fields parameterOffset and parameterCount.
    Signed-off-by: Jakob Brandsborg Ehmsen <jakobehmsen.github@gmail.com>

--- eclipse/src/duro/reflang/ast2/ASTClosure.java
+++ eclipse/src/duro/reflang/ast2/ASTClosure.java
@@ -5,13 +5,9 @@ import java.io.IOException;
 import duro.io.TreeWriter;
 
 public class ASTClosure implements AST {
-	public final int parameterOffset;
-	public final int parameterCount;
 	public final AST body;
 	
-	public ASTClosure(int parameterOffset, int parameterCount, AST body) {
-		this.parameterOffset = parameterOffset;
-		this.parameterCount = parameterCount;
+	public ASTClosure(AST body) {
 		this.body = body;
 	}
 
@@ -23,10 +19,6 @@ public class ASTClosure implements AST {
 	@Override
 	public void writeTo(TreeWriter writer) throws IOException {
 		writer.write("{");
-		writer.write(parameterOffset);
-		writer.write(",");
-		writer.write(parameterCount);
-		writer.write("|");
 		writer.indentnl();
 		body.writeTo(writer);
 		writer.dedentnl();
